2087b763f44799ca3b165e8188dced9a1b0bff16
smithjn <smithjn@g.cofc.edu>
2018-07-27 01:05:43 -0400

    Initial commit

---
+++ src/main/kotlin/EchoServer.kt
@@ -0,0 +1,45 @@
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import java.net.InetSocketAddress
+
+
+class EchoServer(private val port: Int) {
+
+
+    fun start() {
+        val serverHandler = EchoServerHandler()
+        val group = NioEventLoopGroup()
+        try {
+            val b = ServerBootstrap()
+            b.group(group)
+                    .channel(NioServerSocketChannel::class.java)
+                    .localAddress(InetSocketAddress(port))
+                    .childHandler(object : ChannelInitializer<SocketChannel>() {
+                        public override fun initChannel(ch: SocketChannel) {
+                            ch.pipeline().addLast(serverHandler)
+                        }
+                    })
+
+            val f = b.bind().sync()
+            println(EchoServer::class.java.name +
+                    " started and listening for connections on " + f.channel().localAddress())
+            f.channel().closeFuture().sync()
+        } finally {
+            group.shutdownGracefully().sync()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        System.err.println("Usage: " + EchoServer::class.java.simpleName +
+                " <port>"
+        )
+        return
+    }
+    val port = Integer.parseInt(args[0])
+    EchoServer(port).start()
+}
---
+++ src/main/kotlin/EchoServerHandler.kt
@@ -0,0 +1,30 @@
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelFutureListener
+import io.netty.channel.ChannelHandler.Sharable
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.util.CharsetUtil
+
+
+@Sharable
+class EchoServerHandler : ChannelInboundHandlerAdapter() {
+    override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+        val `in` = msg as ByteBuf
+        println(
+                "Server received: " + `in`.toString(CharsetUtil.UTF_8))
+        ctx.write(`in`)
+    }
+
+    @Throws(Exception::class)
+    override fun channelReadComplete(ctx: ChannelHandlerContext) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE)
+    }
+
+    override fun exceptionCaught(ctx: ChannelHandlerContext,
+                                 cause: Throwable) {
+        cause.printStackTrace()
+        ctx.close()
+    }
+}
